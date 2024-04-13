9da769198d2ea4026a6c392c062a364eb15050e1
abundance alex <aeick135@gmail.com>
2024-04-12 22:35:05 -0400

    syntax edits

--- openAiBot/chatBot/openAiBot.py
+++ openAiBot/chatBot/openAiBot.py
@@ -1,10 +1,24 @@
 import os
 from dotenv import load_dotenv
-import openai
+from openai import OpenAI
 
 
 # Load environment variables
 load_dotenv()
 
 # Get openAi key
-openai.api_key = os.getenv('API_KEY')
+client = OpenAI(api_key=os.environ.get("API_KEY"))
+
+#Pass in Model params
+MODEL = "gpt-4"                                                     # delcares & holds model type
+response = client.chat.completions.create(
+    model = MODEL,
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Explain what happened to magelian whe he tried to sail around the world."},
+    ],
+    temperature = 0,
+)
+
+# Print the responce to terminal
+print(response.choices[0].message.content)
