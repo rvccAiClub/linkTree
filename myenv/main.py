20f94abd6556a4c0382f542776578ed0ce0cb66d
austin16399 <austin16399@gmail.com>
2024-04-25 12:30:52 -0400

    embedding data

--- myenv/main.py
+++ myenv/main.py
@@ -14,37 +14,43 @@ client = OpenAI(api_key=os.environ.get("API_KEY"))
 MODEL = "gpt-3.5-turbo"
 
 # Instatiate the chatBot object (we pass in a unique name and gave the model a context point of what it will do.)
-#/clubChatBot = client.beta.assistants.create(
-#    name = "ARVEE",
- #   instructions = """You are a friendly and consise assistant who helps new students answer questions about RVCC's A.I. Club.""",
-  #  model = MODEL
-#)
+clubChatBot = client.beta.assistants.create(
+   name = "ARVEE",
+   instructions = "You are a friendly and consise assistant who helps new students answer questions about RVCC's A.I. Club.",
+   tools = [{"type": "retrieval"}],
+   model = MODEL,
+   file_ids = ["file-mJKto4SStWzndMjjWpaAmMHj"]
 
-#assistant_id = clubChatBot.id
-#print(clubChatBot.id)
+)
+# file = client.files.create(
+#     file = open ("test.txt", "rb"),
+#     purpose = "assistants"
+# )
+# print(file)
 
-# Instantiate the thread object
-#thread = client.beta.threads.create(
- #   messages = [
-  #      {
-   #         "role": "user",
-    #        "content": "What does A.I. club do? I'm interested in joining the club!"
-     #  }
-   #]
-#)
-#thread_id = thread.id
-#print(thread_id)
+# assistant_id = clubChatBot.id
+# print(clubChatBot.id)
 
-# Video timestamp 49:21
+# # Instantiate the thread object
+# thread = client.beta.threads.create(
+#    messages = [
+#        {
+#            "role": "user",
+#            "content": "What does A.I. club do? I'm interested in joining the club!"
+#       }
+#    ]
+# )
+# thread_id = thread.id
+# print(thread_id)
 
 assistant_id = os.environ.get("ASST_ID")
 thread_id = os.environ.get("THREAD_ID")
 
 #===create message===#
 print("")
-print("Hulk: Hey brother, what do you want to know about RVCC's AI Club?")
+print("Arvee: Hey, what do you want to know about RVCC's AI Club?")
 print("")
-print("Say 'Q' to quit chatting with me brother.")
+print("Say 'Q' to quit chatting with me.")
 print("")
 user_message = input("User: ")
 while user_message != "Q":
@@ -58,7 +64,7 @@ while user_message != "Q":
         return client.beta.threads.runs.create(
             thread_id = thread_id,
             assistant_id = assistant_id,
-            instructions = "Please address the user as Hulk Hogan"
+            instructions = "Please address the user as user" 
     )
 
     assistant_message = submit_message(assistant_id, thread_id, user_message)
